560a3e6d4d3baeb14171d5adaa44c086b9ecd7d1
lamix1 <camilacristina328santoss@gmail.com>
2023-10-20 08:36:07 -0300

    Alterando a view de Pedidos Abertos

--- src/views/PedidosAbertosView.vue
+++ src/views/PedidosAbertosView.vue
@@ -1,47 +1,60 @@
 <template>
-  <div class="container position-absolute top-50 start-50 translate-middle">
-    <p class="text-center fs-2">Pedidos Abertos</p>
-    <table class="table">
-      <thead class="table-danger">
-        <tr>
-          <th scope="col">Funcionário</th>
-          <th scope="col">ID</th>
-          <th scope="col">NOME</th>
-          <th scope="col">MESA</th>
-          <th scope="col">PEDIDO</th>
-          <th scope="col">TOTAL</th>
-          <th scope="col">SITUAÇÃO</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">João</th>
-          <td >1</td>
-          <td>Ana</td>
-          <td>2</td>
-          <td>Porção de Batata Frita</td>
-          <td>$25</td>
-          <td><input class="form-check-input" type="checkbox" /></td>
-        </tr>
-        <tr>
-          <th scope="row">Eduarda</th>
-          <td>2</td>
-          <td>Luiza</td>
-          <td>5</td>
-          <td>Chawarma de Frango com Requeijão</td>
-          <td>$28</td>
-          <td><input class="form-check-input" type="checkbox" /></td>
-        </tr>
-        <tr>
-          <th scope="row">Maria</th>
-          <td>3</td>
-          <td>Analu</td>
-          <td>3</td>
-          <td>Chapeado de 500G</td>
-          <td>45$</td>
-          <td><input class="form-check-input" type="checkbox" /></td>
-        </tr>
-      </tbody>
-    </table>
+  <div class="d-flex justify-content-center align-items-center">
+    
+      <div class="card">
+        <div class="card-header text-center py-3">
+          <h5 class="mb-0 text-center">
+            <strong>Pedidos Abertos</strong>
+          </h5>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-hover text-nowrap">
+              <thead>
+                <tr>
+                  <th scope="col"><strong>Funcionário</strong></th>
+                  <th scope="col"><strong>ID</strong></th>
+                  <th scope="col"><strong>Nome</strong></th>
+                  <th scope="col"><strong>Mesa</strong></th>
+                  <th scope="col"><strong>Pedido</strong></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Maria</td>
+                  <td>2</td>
+                  <td>Eduardo</td>
+                  <td>10</td>
+                  <td>Chawarma de Frango</td>
+                </tr>
+                <tr>
+                  <td>Paulo</td>
+                  <td>3</td>
+                  <td>Telma</td>
+                  <td>20</td>
+                  <td>Porção de Calabresa</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    
   </div>
+
 </template>
+
+<style scoped>
+
+.card {
+  width: 80%;
+  height: 80%;
+  border-radius: 30px;
+}
+
+.table {
+  border-radius: 70px;
+  width: 100%;
+  height: 100%;
+}
+</style>
