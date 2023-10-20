a2279bc9641233bc00cf29e1fee148021e9b1aa5
lamix1 <camilacristina328santoss@gmail.com>
2023-10-20 08:43:26 -0300

    Editando a view de cardápio

--- src/views/CardapioView.vue
+++ src/views/CardapioView.vue
@@ -1,35 +1,93 @@
 <template>
-  <div class="container position-absolute top-50 start-50 translate-middle">
-    <p class="text-center fs-2">Cardápio</p>
-    <table class="table">
-      <thead class="table-danger">
+  <div class="d-flex justify-content-center align-items-center">
+    
+    <div class="card">
+      <div class="card-header text-center py-3">
+        <h5 class="mb-0 text-center">
+          <strong>Cardápio</strong>
+        </h5>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-hover text-nowrap">
+            <thead>
         <tr>
-          <th scope="col">ID</th>
-          <th scope="col">TÌTULO</th>
-          <th scope="col">PESO</th>
-          <th scope="col">PREÇO</th>
-          <th scope="col">CATEGORIA</th>
-          <th scope="col">INDISPONIBILIDADE</th>
+          <th scope="col"><strong>ID</strong></th>
+          <th scope="col"><strong>Nome</strong></th>
+          <th scope="col"><strong>Peso</strong></th>
+          <th scope="col"><strong>Preço</strong></th>
+          <th scope="col"><strong>Categoria</strong></th>
+          <th scope="col"><strong>Indisponibilidade</strong></th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <th scope="row">1</th>
-          <td>Batata Frita</td>
+          <td>Porção de Batata Frita</td>
           <td>500g</td>
-          <td>25$</td>
+          <td>R$25,00</td>
           <td>Porção</td>
+          <td><input class="form-check-input" type="checkbox" /></td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Chawarma de Frango</td>
+          <td>250g</td>
+          <td>R$50,00</td>
+          <td>Chawarma</td>
+          <td><input class="form-check-input" type="checkbox" /></td>
+        </tr>
+        <tr>
+          <th scope="row">8</th>
+          <td>Fanta em Lata</td>
+          <td>350ml</td>
+          <td>R$5,00</td>
+          <td>Bebida</td>
           <td><input class="form-check-input" type="checkbox" /></td>
         </tr>
         <tr>
           <th scope="row">2</th>
           <td>Chawarma de Carne</td>
           <td>150g</td>
-          <td>20$</td>
+          <td>R$20,00</td>
           <td>Chawarma</td>
+          <td><input class="form-check-input" type="checkbox" /></td>
+        </tr>
+        <tr>
+          <th scope="row">6</th>
+          <td>Porção de Carne</td>
+          <td>150g</td>
+          <td>R$30,00</td>
+          <td>Porção</td>
+          <td><input class="form-check-input" type="checkbox" /></td>
+        </tr>
+        <tr>
+          <th scope="row">7</th>
+          <td>Coca-Cola em Lata</td>
+          <td>350ml</td>
+          <td>R$5,00</td>
+          <td>Bebida</td>
           <td><input class="form-check-input" type="checkbox" /></td>
         </tr>
       </tbody>
     </table>
   </div>
+  </div>
+</div>
+  </div>
 </template>
+
+<style scoped>
+
+.card {
+  width: 80%;
+  height: 80%;
+  border-radius: 30px;
+}
+
+.table {
+  border-radius: 70px;
+  width: 100%;
+  height: 100%;
+}
+</style>
