b4ee4ef58432b53b206cb1e7bb87fb09ef918542
lamix1 <camilacristina328santoss@gmail.com>
2023-10-20 08:52:12 -0300

    Puxando os pedudos do backend

--- src/views/PedidosAbertosView.vue
+++ src/views/PedidosAbertosView.vue
@@ -1,60 +1,78 @@
 <template>
-  <div class="d-flex justify-content-center align-items-center">
-    
-      <div class="card">
-        <div class="card-header text-center py-3">
-          <h5 class="mb-0 text-center">
-            <strong>Pedidos Abertos</strong>
-          </h5>
-        </div>
-        <div class="card-body">
-          <div class="table-responsive">
-            <table class="table table-hover text-nowrap">
-              <thead>
-                <tr>
-                  <th scope="col"><strong>Funcionário</strong></th>
-                  <th scope="col"><strong>ID</strong></th>
-                  <th scope="col"><strong>Nome</strong></th>
-                  <th scope="col"><strong>Mesa</strong></th>
-                  <th scope="col"><strong>Pedido</strong></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Maria</td>
-                  <td>2</td>
-                  <td>Eduardo</td>
-                  <td>10</td>
-                  <td>Chawarma de Frango</td>
-                </tr>
-                <tr>
-                  <td>Paulo</td>
-                  <td>3</td>
-                  <td>Telma</td>
-                  <td>20</td>
-                  <td>Porção de Calabresa</td>
-                </tr>
-              </tbody>
-            </table>
+  <div>
+    <div class="container">
+      <div v-for="(pedido, index) in pedidos" :key="index">
+        <div class="card">
+          <div class="card-content">
+            <p class="mesa">Mesa # {{ pedido.mesa }}</p>
+            <p class="cliente">Cliente: {{ pedido.cliente }}</p>
+            <p class="total">Total: R$ {{ pedido.total.toFixed(2) }}</p>
+            <p class="status">Status: {{ pedido.status }}</p>
           </div>
         </div>
       </div>
-    
+    </div>
   </div>
-
 </template>
 
-<style scoped>
+<script>
+import axios from 'axios';
 
-.card {
-  width: 80%;
-  height: 80%;
-  border-radius: 30px;
+export default {
+  data() {
+    return {
+      pedidos: [],
+    };
+  },
+  created() {
+    axios
+      .get('http://191.52.55.129:19002/api/pedidos/')
+      .then((response) => {
+        this.pedidos = response.data;
+      })
+      .catch((error) => {
+        console.error('Erro ao buscar os pedidos:', error);
+      });
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  width: 100%;
 }
 
-.table {
-  border-radius: 70px;
-  width: 100%;
-  height: 100%;
+.card {
+  width: 50rem;
+  height: 15rem;
+  margin-bottom: 10px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+.mesa {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.status {
+  font-size: 18px;
+  color: #d32f2f;
+}
+
+.cliente {
+  font-size: 18px;
+}
+
+.total {
+  font-size: 16px;
+  margin-top: 5px;
 }
 </style>
