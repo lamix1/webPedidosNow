755fa0f740d61b681b57574426834a455e85f70a
luizatorrens <luiza.torrens.2006@gmail.com>
2023-11-14 13:54:09 -0300

    Buscando itens do cardápio por nome

--- src/views/CardapioView.vue
+++ src/views/CardapioView.vue
@@ -1,39 +1,48 @@
 <template>
   <div class="d-flex justify-content-center align-items-center">
-    
     <div class="card">
-      <div class="card-header text-center py-3">
+      <div class="card-header text-center py-3 bg-danger text-light">
         <h5 class="mb-0 text-center">
           <strong>Cardápio</strong>
         </h5>
       </div>
-      <div class="card-body">
-        <div class="table-responsive">
+      <div class="card-body m-3">
+        
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control"
+            id="searchInput"
+            v-model="searchTerm"
+            placeholder="Busca por nome"
+          />
+          
+        </div>
+
+        <div class="table-responsive m-3">
           <table class="table table-hover text-nowrap">
             <thead>
-        <tr>
-          <th scope="col"><strong>ID</strong></th>
-          <th scope="col"><strong>Nome</strong></th>
-          <th scope="col"><strong>Peso</strong></th>
-          <th scope="col"><strong>Preço</strong></th>
-          <th scope="col"><strong>Categoria</strong></th>
-          <!-- <th scope="col"><strong>Disponibilidade</strong></th> -->
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in produtos" :key="item.id">
+              <tr>
+                <th scope="col"><strong>ID</strong></th>
+                <th scope="col"><strong>Nome</strong></th>
+                <th scope="col"><strong>Peso</strong></th>
+                <th scope="col"><strong>Preço</strong></th>
+                <th scope="col"><strong>Categoria</strong></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in filteredProdutos" :key="item.id">
                 <th scope="row">{{ item.id }}</th>
-                <td>{{ item.titulo}}</td>
+                <td>{{ item.titulo }}</td>
                 <td>{{ item.gramas }}</td>
                 <td>R$ {{ item.preco }}</td>
                 <td>{{ item.categoria }}</td>
-                <!-- <input class="form-check-input" type="checkbox" v-model="item.disponivel" /></td> -->
               </tr>
-      </tbody>
-    </table>
-  </div>
-  </div>
-</div>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,17 +52,27 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      produtos: [], 
+      produtos: [],
+      searchTerm: '', // Adicionando a propriedade para armazenar o termo de pesquisa
     };
   },
+  computed: {
+    filteredProdutos() {
+      // Filtrando os produtos com base no termo de pesquisa
+      return this.produtos.filter(item =>
+        item.titulo.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    },
+  },
   mounted() {
-    this.getProdutos(); 
+    this.getProdutos();
   },
   methods: {
     getProdutos() {
-      axios.get('https://backendpedidosnow-dev-pabx.1.ie-1.fl0.io/api/produtos/')
+      axios
+        .get('https://backendpedidosnow-dev-pabx.1.ie-1.fl0.io/api/produtos/')
         .then((response) => {
-          this.produtos = response.data; 
+          this.produtos = response.data;
         })
         .catch((error) => {
           console.error('Erro ao buscar os produtos:', error);
@@ -64,7 +83,6 @@ export default {
 </script>
 
 <style scoped>
-
 .card {
   width: 80%;
   height: 80%;
